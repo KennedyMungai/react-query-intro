111f82afa9139d5d4096804655d45ed3d4ca3975
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 10:14:43 +0300

    Added some query statements

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useQuery } from "@tanstack/react-query"
+
 const POSTS = [
   { id: 1, title: "Post 1" },
   { id: 2, title: "Post 2" },
@@ -8,6 +11,21 @@ const wait = (duration: number) => {
 }
 
 export default function Home() {
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => wait(1000).then(() => [...POSTS])
+  })
+
+  const { data, isLoading, isError, error } = postsQuery
+
+  if (isLoading) {
+    return <p className="bg-slate-200 min-w-screen animate-pulse text-red-600 font-bold text-6xl">Loading...</p>
+  }
+
+  if (isError) {
+    return <pre className="bg-slate-200 min-w-screen animate-pulse text-red-600 font-bold text-6xl">{ JSON.stringify(error)}</pre>
+  }
+
   return (
     <main></main>
   )
