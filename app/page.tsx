3d7155a6fb298624b43396c0a00b686f0a250f47
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 10:36:29 +0300

    Made some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,64 @@
 'use client'
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 const POSTS = [
-  { id: 1, title: "Post 1" },
-  { id: 2, title: "Post 2" },
+	{ id: 1, title: 'Post 1' },
+	{ id: 2, title: 'Post 2' }
 ]
 
 const wait = (duration: number) => {
-  return new Promise(resolve => setTimeout(resolve, duration))
+	return new Promise((resolve) => setTimeout(resolve, duration))
 }
 
 export default function Home() {
-  const postsQuery = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => wait(1000).then(() => [...POSTS])
-  })
+	const queryClient = useQueryClient()
 
-  const { data, isLoading, isError, error } = postsQuery
+	const postsQuery = useQuery({
+		queryKey: ['posts'],
+		queryFn: () => wait(1000).then(() => [...POSTS])
+	})
 
-  if (isLoading) {
-    return <p className="bg-slate-200 min-w-screen animate-pulse text-red-600 font-bold text-6xl">Loading...</p>
-  }
+	const newPostMutation = useMutation({
+		mutationFn: (title: string) =>
+			wait(1000).then(() =>
+				POSTS.push({ id: +crypto.randomUUID(), title })
+			),
+		onSuccess: () => {
+			queryClient.invalidateQueries(['posts'])
+		}
+	})
 
-  if (isError) {
-    return <pre className="bg-slate-200 min-w-screen animate-pulse text-red-600 font-bold text-6xl">{ JSON.stringify(error)}</pre>
-  }
+	const { data, isLoading, isError, error } = postsQuery
 
-  return (
-    <main></main>
-  )
+	const { isPending } = newPostMutation
+
+	if (isLoading) {
+		return (
+			<p className='bg-slate-200 min-w-screen animate-pulse text-red-600 font-bold text-6xl'>
+				Loading...
+			</p>
+		)
+	}
+
+	if (isError) {
+		return (
+			<pre className='bg-slate-200 min-w-screen animate-pulse text-red-600 font-bold text-6xl'>
+				{JSON.stringify(error)}
+			</pre>
+		)
+	}
+
+	return (
+		<main>
+			{data?.map((item) => (
+				<div key={item.id}>{item.title}</div>
+			))}
+			<button
+				disabled={isPending}
+				onClick={() => newPostMutation.mutate('New Post')}
+			>
+				Add new
+			</button>
+		</main>
+	)
 }
